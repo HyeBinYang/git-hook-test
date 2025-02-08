28b11dd1db885ade507cfd2804b129bf36de44e1
HyeBinYang <skdisk7368@gmail.com>
2025-02-08 13:18:42 +0900

    feat: main에 'eee' 변수 추가

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const a = 'sdsds';
-const b = 'asdasds';
-const c = 'dfdfdf';
-const dd = 'czzxxz';
+const a = "sdsds";
+const b = "asdasds";
+const c = "dfdfdf";
+const dd = "czzxxz";
+
+const eee = "asdasdasd";
